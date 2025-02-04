c2c7e7a7abc7b53fda45980654cd84c1445eb9c1
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-04 12:10:57 -0800

    remove dribbble import

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -1,4 +1,3 @@
-import IconDribbble from "../components/icons/dribbble.astro";
 import IconGithub from "../components/icons/github.astro";
 import IconLinkedin from "../components/icons/linkedin.astro";
 import IconTwitter from "../components/icons/twitter.astro";
