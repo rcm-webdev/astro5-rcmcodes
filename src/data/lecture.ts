a62ef1b2305a7c93a3650fc674fc283fc79e6582
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-31 21:45:03 -0700

    update lecture

--- src/data/lecture.ts
+++ src/data/lecture.ts
@@ -3,7 +3,7 @@ import type { Props as LectureProp } from "../components/lecture/props";
 export const LECTURE: LectureProp[] = [
   {
     dates: "Speaking Engagement",
-    title: "MVC Simplified",
+    title: "MVC Lecture",
     lectureUrl: "https://100devs.org/",
     description:
       "I have a lecture that covered the Model-View-Controller (MVC) design pattern. Presentation shared with an online community.",
